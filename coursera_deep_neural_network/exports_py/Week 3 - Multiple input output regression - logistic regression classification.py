d410fe529fd1859799cf1a0da75b321ddd24e016
castorfou <guillaume.ramelet@gmail.com>
2021-07-12 15:08:34 +0200

    end of week 3

--- coursera_deep_neural_network/exports_py/Week 3 - Multiple input output regression - logistic regression classification.py
+++ coursera_deep_neural_network/exports_py/Week 3 - Multiple input output regression - logistic regression classification.py
@@ -181,6 +181,239 @@ Yhat = model(X)
 Yhat
 
 
+# # Multiple output linear regression training
+
+# In[8]:
+
+
+from torch.utils.data import Dataset, DataLoader
+
+class Data2D(Dataset):
+    def __init__(self):
+        self.x = torch.zeros(20,2)
+        self.x[:, 0] = torch.arange(-1,1,0.1)
+        self.x[:, 1] = torch.arange(-1,1,0.1)
+        self.w = torch.tensor([ [1.0, -1.0], [1.0, -1.0]])
+        self.b = torch.tensor([[1.0, -1.0]])
+        self.f = torch.mm(self.x, self.w)+self.b
+        self.y = self.f + 0.1*torch.randn((self.x.shape[0], 1))
+        self.len = self.x.shape[0]
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    def __len__(self):
+        return self.len
+
+
+# In[10]:
+
+
+from torch import nn, optim
+
+data_set = Data2D()
+criterion = nn.MSELoss()
+trainloader = DataLoader(dataset=data_set, batch_size=1)
+model = LR(input_size=2, output_size=2)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+
+# In[11]:
+
+
+for epoch in range(100):
+    for x, y in trainloader:
+        yhat = model(x)
+        loss = criterion(yhat, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+
+
+# # Linear Classifier and Logistic Regression
+
+# # Logisitic Regression: Prediction
+
+# ## Logistic function
+
+# In[2]:
+
+
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+z = torch.arange(-100, 100, 0.1).view(-1, 1)
+sig = nn.Sigmoid()
+yhat = sig(z)
+plt.plot(z.numpy(), yhat.numpy())
+
+
+# In[4]:
+
+
+import torch
+
+z = torch.arange(-100, 100, 0.1).view(-1, 1)
+yhat = torch.sigmoid(z)
+plt.plot(z.numpy(), yhat.numpy())
+
+
+# [torch.nn.Sigmoid vs torch.sigmoid - PyTorch Forums](https://discuss.pytorch.org/t/torch-nn-sigmoid-vs-torch-sigmoid/57691/3)
+# 
+# > `torch.nn.Sigmoid` (note the capital “S”) is a _class_. When you  
+# > instantiate it, you get a _function object_, that is, an object that you  
+# > can call like a function. In contrast, `torch.sigmoid` is a function.
+
+# ## nn.Sequential
+
+# ![image.png](attachment:image.png)
+
+# ## nn.Module
+
+# In[6]:
+
+
+import torch.nn as nn
+
+class logistic_regression(nn.Module):
+    def __init__(self, in_size):
+        super(logistic_regression, self).__init__()
+        self.linear = nn.Linear(in_size, 1)
+    def forward(self, x):
+        z = torch.sigmoid(self.linear(x))
+        return z
+
+
+# In[11]:
+
+
+custom_model = logistic_regression(1)
+x = torch.tensor([[1.0]])
+
+yhat = custom_model(x)
+yhat
+
+
+# In[12]:
+
+
+sequential_model = nn.Sequential(nn.Linear(1,1), nn.Sigmoid())
+sequential_model(x)
+
+
+# ## Making a prediction
+
+# In[13]:
+
+
+print(list(custom_model.parameters()))
+
+
+# In[14]:
+
+
+print((list(sequential_model.parameters())))
+
+
+# In[17]:
+
+
+x=torch.tensor([[1.0], [2.0]])
+custom_model(x)
+
+
+# In[18]:
+
+
+sequential_model(x)
+
+
+# ## Multidimensional Logistic Regression
+
+# In[19]:
+
+
+custom_2D_model = logistic_regression(2)
+sequential_2D_model = nn.Sequential(nn.Linear(2, 1), nn.Sigmoid())
+
+
+# In[20]:
+
+
+print(list(sequential_2D_model.parameters()))
+
+
+# In[22]:
+
+
+x=torch.tensor([[1.0, 2.0]])
+yhat = sequential_2D_model(x)
+yhat
+
+
+# # Logistic Regression Cross Entropy Loss
+
+# In[7]:
+
+
+import torch
+from torch.utils.data import Dataset
+
+class Data(Dataset):
+    def __init__(self):
+        self.x = torch.arange(-3, 3, 0.1).view(-1, 1)
+        self.y = -3*self.x+1
+        self.len = self.x.shape[0]
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    def __len__(self):
+        return self.len
+    
+dataset = Data()
+
+
+# In[8]:
+
+
+from torch.utils.data import DataLoader
+
+trainloader = DataLoader(dataset=dataset, batch_size=1)
+
+
+# In[10]:
+
+
+import torch.nn as nn
+
+model = nn.Sequential(nn.Linear(1, 1), nn.Sigmoid())
+
+
+# In[11]:
+
+
+from torch import optim
+
+optimizer = optim.SGD(model.parameters(), lr = 0.01)
+
+
+# In[12]:
+
+
+criterion = nn.BCELoss()
+
+
+# In[13]:
+
+
+for epoch in range(100):
+    for x, y in trainloader:
+        yhat = model(x)
+        loss = criterion(yhat, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
 # In[ ]:
 
 
